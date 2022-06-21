62eef8232540a8383ec11cdb5f895dffa5f619d8
algoroot <algoroot524@gmail.com>
2022-06-21 13:06:43 +0900

    Type Alias

---
+++ Ellie/TypeScript/1-types/1-4-alias.ts
@@ -0,0 +1,31 @@
+{
+  /**
+   * Type Aliases
+   */
+  // Text라는 새로운 타입 선언
+  type Text = string;
+  const name: Text = "Kelly";
+  const address: Text = "korea";
+  type Num = number;
+  // object 도 타입으로 만들 수 있다.
+  type Student = {
+    name: string;
+    age: number;
+  };
+  const student: Student = {
+    // animal: "dog",
+    name: "kelly",
+    age: 27,
+  };
+
+  /**
+   * String Literal Types
+   */
+
+  type Name = "name";
+  let kellyName: Name;
+  // kellyName = "dfdf";
+  kellyName = "name";
+  type JSON = "json";
+  const json: JSON = "json";
+}
