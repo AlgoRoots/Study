bf3a768c2fd8e474abfd05c1a7d4746c929745ec
algoroot <algoroot524@gmail.com>
2022-06-21 15:35:52 +0900

    Discriminated Union : 공통적인 property 가지기

--- Ellie/TypeScript/1-types/1-6-discriminated.ts
+++ Ellie/TypeScript/1-types/1-6-discriminated.ts
@@ -1,43 +1,36 @@
 {
-  /**
-   *  Union Types: OR
-   */
-  type Direction = "left" | "right" | "up" | "down";
-  function move(direction: Direction) {
-    console.log(direction);
-  }
-  move("down");
-
-  type TileSize = 8 | 16 | 32;
-  const tile: TileSize = 16;
-
-  // function: login -> success, fail ⏱
+  // 어떤 케이스든 공통적인 property를 가지고 있음으로서 조금 더 구분하기 쉽게 만든다.
+  // function: login -> success, fail
   type SuccessState = {
+    result: "success";
     response: {
       body: string;
     };
   };
   type FailState = {
+    result: "fail";
     reason: string;
   };
   type LoginState = SuccessState | FailState;
 
   function login(): LoginState {
     return {
+      result: "success",
       response: {
         body: "logged in!",
       },
     };
   }
-
   // printLoginState(state: LoginState)
   // success -> 🎉 body
   // fail -> 😭 reason
-  function printLoginState(state: LoginState) {
-    if ("response" in state) {
+  function printLoginState(state: LoginState): void {
+    // if ("response" in state) {
+    // state.result // success or fail
+    if (state.result === "success") {
       console.log(`🎉 ${state.response.body}`);
     } else {
-      console.log(`😭 ${state.reason}`);
+      console.log(`😭  ${state.reason}`);
     }
   }
 }
