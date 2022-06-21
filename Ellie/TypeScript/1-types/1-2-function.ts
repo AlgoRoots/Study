5ed39b3fd8f37441de4176f31e74b8aecf063ba9
algoroot <algoroot524@gmail.com>
2022-06-21 10:59:32 +0900

    함수에서 타입 이용하기 ( JS > TS )

---
+++ Ellie/TypeScript/1-types/1-2-function.ts
@@ -0,0 +1,31 @@
+{
+  // JavaScript 💩
+  function jsAdd(num1, num2) {
+    return num1 + num2;
+  }
+
+  // TypeScript
+  function add(num1: number, num2: number): number {
+    return num1 + num2;
+  }
+
+  // JavaScript 💩
+  function jsFetchNum(id) {
+    // code ...
+    // code ...
+    // code ...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
+
+  // TypeScript 직관적이다. 어떤타입을 리턴하는지 알 수 있다 .
+  function fetchNum(id: string): Promise<number> {
+    // code ...
+    // code ...
+    // code ...
+    return new Promise((resolve, reject) => {
+      resolve(100);
+    });
+  }
+}
