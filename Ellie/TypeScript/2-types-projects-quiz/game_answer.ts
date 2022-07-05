6280a49180d15a4ee710a0883891f54ec7251f23
algoroot <algoroot524@gmail.com>
2022-07-05 16:13:54 +0900

    Coordinate game answer: const 선언, switch문에서 return 아닌 break 사용

--- Ellie/TypeScript/2-types-projects-quiz/game_answer.ts
+++ Ellie/TypeScript/2-types-projects-quiz/game_answer.ts
@@ -2,34 +2,37 @@
  * Let's make a game 🕹
  */
 
-let position: { x: number; y: number } = {
-  x: 0,
-  y: 0,
-};
-
-console.log(position); // { x: 0, y: 0}
-move_me("up");
-console.log(position); // { x: 0, y: 1}
-move_me("down");
-console.log(position); // { x: 0, y: 0}
-move_me("left");
-console.log(position); // { x: -1, y: 0}
-move_me("right");
-console.log(position); // { x: 0, y: 0}
+const position = { x: 0, y: 0 };
 
 type Direction = "up" | "down" | "left" | "right";
 
-function move_me(direction: Direction) {
+function move(direction: Direction) {
+  // 함수에서 별도로 값을 리턴하지 않는다면 (리턴이 void type) break를 사용해도 됨.
+  // break작성 안하면 밑에까지 다 실행됨
   switch (direction) {
     case "up":
-      return position.y++;
+      position.y += 1;
+      break;
     case "down":
-      return position.y--;
+      position.y -= 1;
+      break;
     case "left":
-      return position.x--;
+      position.x -= 1;
+      break;
     case "right":
-      return position.x++;
+      position.x += 1;
+      break;
     default:
-      throw Error("unknown direction");
+      throw Error(`unknown direction : ${direction}`);
   }
 }
+
+console.log(position); // { x: 0, y: 0}
+move("up");
+console.log(position); // { x: 0, y: 1}
+move("down");
+console.log(position); // { x: 0, y: 0}
+move("left");
+console.log(position); // { x: -1, y: 0}
+move("right");
+console.log(position); // { x: 0, y: 0}
