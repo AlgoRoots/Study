bf51709a6e339ada9a5e53b108ccfa69c4a45616
algoroot <algoroot524@gmail.com>
2022-07-05 16:34:03 +0900

    change function name : duplicate function implementation

--- Ellie/TypeScript/2-types-projects-quiz/game_answer.ts
+++ Ellie/TypeScript/2-types-projects-quiz/game_answer.ts
@@ -6,7 +6,7 @@ const position = { x: 0, y: 0 };
 
 type Direction = "up" | "down" | "left" | "right";
 
-function move(direction: Direction) {
+function moveAnswer(direction: Direction) {
   // 함수에서 별도로 값을 리턴하지 않는다면 (리턴이 void type) break를 사용해도 됨.
   // break작성 안하면 밑에까지 다 실행됨
   switch (direction) {
@@ -28,11 +28,11 @@ function move(direction: Direction) {
 }
 
 console.log(position); // { x: 0, y: 0}
-move("up");
+moveAnswer("up");
 console.log(position); // { x: 0, y: 1}
-move("down");
+moveAnswer("down");
 console.log(position); // { x: 0, y: 0}
-move("left");
+moveAnswer("left");
 console.log(position); // { x: -1, y: 0}
-move("right");
+moveAnswer("right");
 console.log(position); // { x: 0, y: 0}
