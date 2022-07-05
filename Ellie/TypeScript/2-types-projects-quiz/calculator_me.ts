1fdacadbd5fa94857376a50744b5ea083d986429
algoroot <algoroot524@gmail.com>
2022-07-05 15:29:24 +0900

    calculator 함수 이름 변경

--- Ellie/TypeScript/2-types-projects-quiz/calculator_me.ts
+++ Ellie/TypeScript/2-types-projects-quiz/calculator_me.ts
@@ -4,7 +4,7 @@
 
 type Method = "add" | "subtract" | "multiply" | "divide" | "remainder";
 
-function calculate(method: Method, num1: number, num2: number): number {
+function calculate_me(method: Method, num1: number, num2: number): number {
   if (method === "add") {
     return num1 + num2;
   }
@@ -22,8 +22,8 @@ function calculate(method: Method, num1: number, num2: number): number {
   }
   return 0; // 👈️ all code paths return a value
 }
-console.log(calculate("add", 1, 3)); // 4
-console.log(calculate("subtract", 3, 1)); // 2
-console.log(calculate("multiply", 4, 2)); // 8
-console.log(calculate("divide", 4, 2)); // 2
-console.log(calculate("remainder", 5, 2)); // 1
+console.log(calculate_me("add", 1, 3)); // 4
+console.log(calculate_me("subtract", 3, 1)); // 2
+console.log(calculate_me("multiply", 4, 2)); // 8
+console.log(calculate_me("divide", 4, 2)); // 2
+console.log(calculate_me("remainder", 5, 2)); // 1
